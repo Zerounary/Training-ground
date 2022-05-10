4bf1d04fd1fddf201a4e0a1bccc42dafb2d50851
Zerounary <850657663@qq.com>
2022-05-10 10:30:13 +0800

    展示小鹤音行的音行按键

--- src/components.d.ts
+++ src/components.d.ts
@@ -4,7 +4,6 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     XiaoHeKeyboard: typeof import('./components/XiaoHeKeyboard.vue')['default']
   }
 }
---
+++ src/components/XiaoHeKeyboard.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>小鹤</h1>
+  <div v-for="(row, index) in rows" :key="index" class="flex relative" :class="`ml-${index * 50}px`">
+    <div v-for="key in row" :key="key.key" class="border h-100px w-100px relative">
+      <img class="absolute top-0 right-0 -z-1 " :src="`/src/assets/xh/${key.key}.png`" />
+      <div class="text-3xl">{{ key.key.toUpperCase() }}</div>
+      <div class="text-xl">{{ key.vs }}</div>
+      <div>{{ key.ve }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+let rowOne =
+  'q-q-iu,w-w-ei,e-e-e,r-r-uan,t-t-ue,y-y-un,u-sh-u,i-ch-i,o-o-uo:o,p-p-ie';
+let rowTwo =
+  'a-a-a,s-s-ong:iong,d-d-ai,f-f-en,g-g-eng,h-h-ang,j-j-an,k-k-uai:ing,l-l-uang:iang';
+let rowThree = 'z-z-ou,x-x-ua:ia,c-c-ao,v-zh-ui:u,b-b-in,n-n-iao,m-m-ian';
+let wrap = (e: string) => {
+  let [key, vs, ve] = e.split('-');
+  return {
+    key,
+    vs,
+    ve,
+  };
+};
+let rows = [
+  rowOne.split(',').map(e => wrap(e)),
+  rowTwo.split(',').map(e => wrap(e)),
+  rowThree.split(',').map(e => wrap(e)),
+];
+
+</script>
+
+<style scoped></style>
