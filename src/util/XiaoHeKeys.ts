720c609d14a110e10e6966a5f001d993548c6ada
Zerounary <850657663@qq.com>
2022-05-13 09:45:02 +0800

    修复数据缺少，的问题

--- src/util/XiaoHeKeys.ts
+++ src/util/XiaoHeKeys.ts
@@ -1,7 +1,7 @@
 let data =
   'q-q-iu,w-w-ei,e-e-e,r-r-uan,t-t-ue,y-y-un,u-sh-u,i-ch-i,o-o-uo:o,p-p-ie,a-a-a,s-s-ong:iong,d-d-ai,f-f-en,g-g-eng,h-h-ang,j-j-an,k-k-uai:ing,l-l-uang:iang,z-z-ou,x-x-ua:ia,c-c-ao,v-zh-ui:ü,b-b-in,n-n-iao,m-m-ian';
 let words =
-  '握-wo,若-ro,托-to,哟-yo说-uo,戳-io,喔-oo,破-po,奥-ao,所-so,多-do,佛-fo,过-go,或-ho,扩-ko,末-mo,做-zo,错-co,桌-vo,拨-bo,挪-no,末-mo,穷-qs,容-rs,同-ts,用-ys,冲-is,送-ss,动-ds,共-gs,红-hs,空-ks,龙-ls,总-zs,熊-xs,从-cs,中-vs,弄-ns,肉-rz,头-tz,有-yz,受-uz,抽-iz,剖-pz,搜-sz,都-dz,否-fz,够-gz,后-hz,口-kz,楼-lz,走-zz,凑-cz,周-vz,某-mz';
+  '握-wo,若-ro,托-to,哟-yo,说-uo,戳-io,喔-oo,破-po,奥-ao,所-so,多-do,佛-fo,过-go,或-ho,扩-ko,末-mo,做-zo,错-co,桌-vo,拨-bo,挪-no,末-mo,穷-qs,容-rs,同-ts,用-ys,冲-is,送-ss,动-ds,共-gs,红-hs,空-ks,龙-ls,总-zs,熊-xs,从-cs,中-vs,弄-ns,肉-rz,头-tz,有-yz,受-uz,抽-iz,剖-pz,搜-sz,都-dz,否-fz,够-gz,后-hz,口-kz,楼-lz,走-zz,凑-cz,周-vz,某-mz';
 
 export interface KeyElement {
   key: string;
