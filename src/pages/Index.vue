2a556e5837bd540e680f8e92762ff175abbe7046
Zerounary <850657663@qq.com>
2022-05-25 11:47:49 +0800

    分不同页展示功能

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <!-- <XiaoHeKeyboard /> -->
-    <EnglishWords />
+    <div class="nav">
+      <router-link class="nav-menu" to="/">英语单词复习</router-link>
+      <router-link class="nav-menu" to="/xiaohe" >小鹤输入法</router-link>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -10,5 +13,17 @@
 </script>
 
 <style scoped>
+
+.nav {
+@apply w-full h-50px  flex items-center space-x-3
+}
+
+.nav-menu {
+  @apply text-xl h-full flex items-center p-3 rounded border
+}
+
+.active {
+  @apply bg-gray-500 text-white;
+}
 
 </style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <EnglishWords />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
